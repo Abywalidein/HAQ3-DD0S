d38b16528c7d0cea83cbda8ecebf05618e0269b7
Abywalidein <mfath6961@gmail.com>
2024-12-17 16:47:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-tools==57.0.0 --force-reinstall
-pip install wheel==0.36.2 --force-reinstall
-pip uninstall comtypes
-pip install --no-cache-dir comtypes
+import os
+c = input("Choose your environment: [0] pip / [1] pip3 : ")
+
+if c == "0":
+    os.system("pip install wheel==0.36.2 --force-reinstall")
+    os.system("pip uninstall comtypes")
+    os.system("pip install --no-cache-dir comtypes")
+     undetected_chromedriver")
+elif c == "1":
+os.system("pip install wheel==0.36.2 --force-reinstall")
+    os.system("pip uninstall comtypes")
+    os.system("pip install --no-cache-dir comtypes")
+     undetected_chromedriver")
+print("Done.")
