06a36e84bd81d47d33f801add575cc79fbc16998
Abywalidein <mfath6961@gmail.com>
2024-12-17 18:25:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import os
 c = input("Choose your environment: [0] pip / [1] pip3 : ")
 
 if c == "0":
-    os.system("pip install wheel==0.36.2 --force-reinstall")
-    os.system("pip uninstall comtypes")
-    os.system("pip install --no-cache-dir comtypes")
+    os.system("pip install requests")
+    os.system("pip install asyncio")
+    os.system("pip install ")
 elif c == "1":
     os.system("pip install wheel==0.36.2 --force-reinstall")
     os.system("pip uninstall comtypes")
